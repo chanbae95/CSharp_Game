3af7e9044683305a18f4fb29829b2b48d63556e0
Chanbae <dlcksqo01@gmail.com>
2022-05-12 23:16:39 +0900

    정수

--- Program.cs
+++ Program.cs
@@ -4,11 +4,43 @@ namespace CSharp
 {
     class Program
     {
+        // 주석
+        /* 
+        여러줄 주석
+         */
         static void Main(string[] args)
         {
+            // 정수형식
+            // byte(1바이트 0~255), short(2바이트 -3만~3만), int(4바이트 -21억~21억), long(8바이트)
+            // sbyte(1바이트 -128~127), ushort(2바이트 0~6만), uint(4바이트 0~43억), ulong(8바이트)zr
+            int hp;
+            short level = 100;
+            long id;
+            hp = 100;
+
+            byte attack = 0;
+            attack--;
+
+            Console.WriteLine("Hellow Number ! {0}", hp);
+            Console.WriteLine(hp);
+            Console.WriteLine(attack);
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hi");
             Console.WriteLine("Bye");
+
+            // 10진수
+            // 0 1 2 3 4 5 6 7 8 9 10
+
+            // 2진수
+            // 0 ~ 1
+            // 0b00 0b01(1) 0b10(2) 0b11(3) 0b100(4)
+
+            // 16진수
+            // 0 ~ 9 a b c d e f
+            // 0x00 0x01 0x02 .. 0x0F(15) 0x10(16)
+
+            // 2진수 16진수로 변환
+            // 0b10001111 => 0x 1000 1111 => 0x8F
         }
     }
 }
