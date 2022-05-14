b214b26c3745df902e796406151d83635a6d5e89
Chanbae <dlcksqo01@gmail.com>
2022-05-14 23:54:25 +0900

    type

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace CSharp
             // 정수형식
             // byte(1바이트 0~255), short(2바이트 -3만~3만), int(4바이트 -21억~21억), long(8바이트)
             // sbyte(1바이트 -128~127), ushort(2바이트 0~6만), uint(4바이트 0~43억), ulong(8바이트)zr
-            int hp;
+            //int hp;
             short level = 100;
             long id;
             hp = 100;
@@ -21,8 +21,8 @@ namespace CSharp
             byte attack = 0;
             attack--;
 
-            Console.WriteLine("Hellow Number ! {0}", hp);
-            Console.WriteLine(hp);
+            //Console.WriteLine("Hellow Number ! {0}", hp);
+           // Console.WriteLine(hp);
             Console.WriteLine(attack);
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hi");
@@ -41,6 +41,36 @@ namespace CSharp
 
             // 2진수 16진수로 변환
             // 0b10001111 => 0x 1000 1111 => 0x8F
+
+            bool b;
+            // 1바이트 크기 임(참/거짓)
+            b = true;
+            b = false;
+
+
+            // 소수 
+            // 4.0 1.3
+            float f = 3.14f;
+            // 4바이트
+            
+            double d = 3.14;
+            // 8바이트
+
+
+            char c = 'a';
+            // 2바이트
+
+            string str = "Hello World string";
+
+
+            Console.WriteLine(str);
+
+
+
+            int hp = 100;
+
+
+
         }
     }
 }
