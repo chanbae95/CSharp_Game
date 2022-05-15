22df519bcc63359a9ee59dd48677c6a642f9ccce
Chanbae <dlcksqo01@gmail.com>
2022-05-15 20:43:38 +0900

    if문

--- Program.cs
+++ Program.cs
@@ -67,9 +67,17 @@ namespace CSharp
 
 
 
-            int hp = 100;
+            int choice = 0; // 0:가위 1:바위 2:보
 
-
+            if(choice == 0) {
+                Console.WriteLine("가위입니다.");
+            }
+            else if (choice == 1) {
+                Console.WriteLine("바위입니다.");
+            }
+            else {
+                Console.WriteLine("보입니다.");
+            }
 
         }
     }
