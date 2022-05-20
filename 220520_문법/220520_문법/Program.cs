eeea3257da57f50a01d829c86e630d290d649929
Chanbae <dlcksqo01@gmail.com>
2022-05-20 22:07:11 +0900

    함수, 연습문제

---
+++ 220520_문법/220520_문법/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        // Method / Function(함수)
+        // 한정자 반환형식 이름(매개변수목록)
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hellow World");
+        }
+
+        // 덧셈 함수
+        // 함수 오버로딩, 함수 이름의 재사용
+        // 반환형식은 오버로딩 구분에 들어가지 않음
+        static int Add(int firstNum, int seconNum)
+        {
+            Console.WriteLine("Add int호출");
+            return firstNum + seconNum;
+        }
+
+        static float Add(float firstNum, float seconNum)
+        {
+            Console.WriteLine("Add float호출");
+            return firstNum + seconNum;
+        }
+
+        static void AddOne(ref int a)
+        {
+            a++;
+        }
+
+        static void Divide(int a, int b, out int c, out int d)
+        {
+            c = a / b;
+            d = a % b;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            int total = Add(5, 10);
+            Console.WriteLine(total);
+            Console.WriteLine(Add(6,7));
+
+            //----------------------
+            int a = 0;
+            AddOne(ref a);
+            Console.WriteLine(a);
+
+            //----------------------
+            int c = 10;
+            int d = 3;
+
+            int result1;
+            int result2;
+
+            Divide(c, d, out result1, out result2);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+
+            //
+            int ret = Add(4, 7);
+            Console.WriteLine(ret);
+
+            float ret2 = Add(2.1f, 3.0f);
+            Console.WriteLine(ret2);
+
+            Console.WriteLine("8888888888888888888888888");
+            Console.WriteLine(3 + 3.1f);
+         }
+    }
+}
---
+++ 220520_연습문제/220520_연습문제/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static int Factorial(int a)
+        {
+            //5
+            int result = 1;
+            for(int i = a; i > 0; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            // 1. 구구단 출력
+            for(int i = 2; i < 10; i++)
+            {
+                for(int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " = " + i*j);
+                }
+            }
+
+            // 2. 별찍기
+            // ver1
+            for(int i = 0; i < 5; i++)
+            {
+               for(int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            // ver2
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i < j)
+                        continue;
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            // 3. Fctorial 함수 만들기
+            int result = Factorial(5);
+            Console.WriteLine(result);
+
+            //
+
+        }
+    }
+}
