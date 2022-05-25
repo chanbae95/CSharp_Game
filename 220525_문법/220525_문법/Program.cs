b73a76c8df6f11c3a807a664f75e0090abb687fc
Chanbae <dlcksqo01@gmail.com>
2022-05-25 20:54:36 +0900

    생성자

---
+++ 220525_문법/220525_문법/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CSharp
+{
+
+    class Knight
+    {
+        public int hp;
+        public int attack;
+
+        //생성자
+        public Knight()
+        {
+            hp = 100;
+            attack = 10;
+            Console.WriteLine("생성자 호출");
+        }
+
+        public Knight(int hp) : this()
+        {
+            this.hp = hp;
+            Console.WriteLine("int 생성자 호출");
+        }
+
+        public Knight Clone()
+        {
+            Knight knight = new Knight();
+            knight.hp = this.hp;
+            knight.attack = this.attack;
+            return knight;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Knight Move");
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine("Knight Acttack");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+            Knight knight2 = new Knight(50);
+
+
+        }
+    }
+}
---
+++ note/note/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            1. a = a++;
+            2. a = ++a;
+            Console.WriteLine(a);
+
+        }
+    }
+}
