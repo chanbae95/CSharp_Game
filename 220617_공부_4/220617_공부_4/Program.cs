bca8448f8b1d86497f9ef408ec1a44f87e4da8ec
Chanbae <dlcksqo01@gmail.com>
2022-06-18 02:27:34 +0900

    delegate

---
+++ 220617_공부_4/220617_공부_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        delegate int OnClicked();
+
+        static void ButtonPressed(OnClicked clickedFunction /*함수자체를 인자로 넘겨주고*/)
+        {
+            //함수로 호출();
+            clickedFunction();
+        }
+        static void Main(string[] args)
+        {
+            // delegate(대리자)
+
+            ButtonPressed(/* */);
+
+        }
+    }
+}
