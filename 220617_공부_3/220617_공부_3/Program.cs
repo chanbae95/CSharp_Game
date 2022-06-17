2ffa619bb0efd585e4591698cf174e5976bc856e
Chanbae <dlcksqo01@gmail.com>
2022-06-18 00:46:51 +0900

    Property(프로퍼티)

---
+++ 220617_공부_3/220617_공부_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Knight
+    {
+        protected int hp;
+
+        // 프로퍼티
+        public int Hp
+        {
+            get { return hp; }
+            set { hp = value; }
+        }
+
+        // 더 줄인 문법
+        // public int Hp { get; set; } = 100;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 프로퍼티
+            Knight knight = new Knight();
+            knight.Hp = 100;
+            int hp = knight.Hp;
+
+        }
+    }
+}
