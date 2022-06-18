c2d0d51ddbe3d484f25f134ba3928a11443ef594
Chanbae <dlcksqo01@gmail.com>
2022-06-18 23:56:27 +0900

    Nullable (널러블)

---
+++ 220618_공부_5/220618_공부_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static int Find()
+        {
+
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            //Nullable 
+
+            int? number = null;
+
+            // number값이 null이 아니면 number값이 들어가고, null이면 0값이 들어감
+            int b = number ?? 0;
+
+            //number = 3;
+            int a = number.Value;
+
+            Console.WriteLine(a);
+        }
+    }
+}
