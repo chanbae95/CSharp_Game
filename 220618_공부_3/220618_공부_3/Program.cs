0f617e68388b8571b1dc3ee874aa10e45ea380f3
Chanbae <dlcksqo01@gmail.com>
2022-06-18 23:18:04 +0900

    Exception(예외처리)

---
+++ 220618_공부_3/220618_공부_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // 1. 0으로 나눌 때
+                // 2. 잘못된 메모리를 참조
+                // 3. 오버플로우
+
+                int a = 10;
+                int b = 0;
+                int result = a / b;
+            }
+            catch (Exception e)
+            {
+
+
+            }
+            finally
+            {
+                //무조건 실행되어야하는 코드 입력
+            }
+
+        }
+    }
+}
