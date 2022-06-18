c3c8fef7de606828e8b4722af7fb9fab5e0d5682
Chanbae <dlcksqo01@gmail.com>
2022-06-18 23:41:45 +0900

    Reflection (리플렉션)

---
+++ 220618_공부_4/220618_공부_4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+namespace CSharp
+{
+    class Program
+    {
+        class Monster
+        {
+            public int hp;
+            protected int attack;
+            private float speed;
+
+            void Attack() { }
+        }
+        static void Main(string[] args)
+        {
+            // Reflection : X-Ray
+
+            Monster monster = new Monster();
+            Type type = monster.GetType();
+
+            var fields = type.GetFields(System.Reflection.BindingFlags.Public 
+                | System.Reflection.BindingFlags.NonPublic
+                | System.Reflection.BindingFlags.Static
+                | System.Reflection.BindingFlags.Instance);
+
+            foreach(FieldInfo field in fields)
+            {
+                string access = "protected";
+                if (field.IsPublic)
+                    access = "public";
+                else if (field.IsPrivate)
+                    access = "private";
+
+                Console.WriteLine(access + field.FieldType.Name + field.Name);
+            }
+
+
+
+
+        }
+    }
+}
