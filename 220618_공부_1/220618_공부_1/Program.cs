81d3ac17e50199d9e18c2aef93297acbeddd3f43
Chanbae <dlcksqo01@gmail.com>
2022-06-18 17:04:02 +0900

    Event

---
+++ 220618_공부_1/220618_공부_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void OnInputTest()
+        {
+            Console.WriteLine("input received");
+        }
+
+        static void Main(string[] args)
+        {
+            inputManager inputManager = new inputManager();
+
+            inputManager.InputKey += OnInputTest;
+
+            while (true)
+            {
+                inputManager.Update();
+            }
+
+        }
+    }
+}
---
+++ 220618_공부_1/220618_공부_1/inputManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    //Observer Pattern
+    class inputManager
+    {
+        public delegate void OnInputKey();
+        public event OnInputKey InputKey;
+
+        public void Update()
+        {
+            if (Console.KeyAvailable == false)
+                return;
+
+            ConsoleKeyInfo info = Console.ReadKey();
+            if(info.Key == ConsoleKey.A)
+            {
+                //모두에게 알림
+                InputKey();
+            }
+        }
+    }
+}
