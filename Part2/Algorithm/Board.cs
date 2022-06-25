a5d92c2a16970dfaf51d79ae8a991aa3d9bd6011
Chanbae <dlcksqo01@gmail.com>
2022-06-25 21:55:46 +0900

    자료구조,알고리즘 1일차

---
+++ Part2/Algorithm/Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Board
+    {
+        public int[] _data = new int[25];
+        public List<int> _data2 = new List<int>();
+        public LinkedList<int> _data3 = new LinkedList<int>();
+
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
---
+++ Part2/Algorithm/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            //커서 끄기
+
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+
+            int lastTick = 0;
+            
+            while (true)
+            {
+                #region 프레임 관리
+                // FPS 프레임
+                int currentTick = System.Environment.TickCount;
+                //시스템이 켜진 후 얼마나 시간이 지났는지 저장(밀리세컨드 단위)
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+                #endregion
+                // 입력
+
+                // 로직
+
+                // 렌더링
+                Console.SetCursorPosition(0, 0);
+
+                for(int i = 0; i < 25; i++)
+                {
+                    for(int j = 0; j < 25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
