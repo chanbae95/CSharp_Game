62c8fec01847d1f47f9e467d1fc4b4a0ee6fefdb
이찬배노트북 <dlcksqo@naver.com>
2022-07-02 20:52:36 +0900

    맵 만들기 알고리즘

--- Part2/Algorithm/Board.cs
+++ Part2/Algorithm/Board.cs
@@ -6,77 +6,170 @@ using System.Threading.Tasks;
 
 namespace Algorithm
 {
-    class Room<T>
-    {
-        public T Data;
-        public Room<T> Next;
-        public Room<T> Prev;
-
-    }
-
-    class RoomList<T>
-    {
-        public Room<T> Head = null;
-        public Room<T> Tail = null;
-        public int Count = 0;
-
-        public Room<T> AddLast(T data)
-        {
-            Room<T> newRoom = new Room<T>();
-            newRoom.Data = data;
-
-            if (Head == null)
-                Head = newRoom;
-
-            //기존의 마지막 방과 새로 추가되는 방을 연결해 준다
-            if(Tail != null)
-            {
-                Tail.Next = newRoom;
-                newRoom.Prev = Tail;
-            }
-            //새로 추가되는 방을 마지막 방으로 인정한다.
-            Tail = newRoom;
-            Count++;
-            return newRoom;
-        }
-
-        public void Remove(Room<T> room)
-        {
-            //첫번째 방 삭제일때
-            if (Head == room)
-                Head = Head.Next;
-
-            // 마지막 방 삭제일때
-            if (Tail == room)
-                Tail = Tail.Prev;
-
-            if (room.Prev != null)
-                room.Prev.Next = room.Next;
-
-            if (room.Next != null)
-                room.Next.Prev = room.Prev;
-
-            Count--;
-
-        }
-
-    }
-
     class Board
     {
-        public int[] _data = new int[25]; // 배열
-        public RoomList<int> _data3 = new RoomList<int>(); // 연결 리스트
-
-
-        public void Initialize()
+        const char CIRCLE = '\u25cf';
+        public Tiletype[,] _tile;
+        public int _size;
+        public enum Tiletype
         {
-            _data3.AddLast(101);
-            _data3.AddLast(102);
-            Room<int> node =  _data3.AddLast(103);
-            _data3.AddLast(104);
-            _data3.AddLast(105);
+            Empty,
+            Wall,
+        }
+        public void Initialize(int size)
+        {
+            if (size % 2 == 0)
+                return;
 
-            _data3.Remove(node);
+            _tile = new Tiletype[size, size];
+            _size = size;
+
+            //GenerateByBinaryTree();            
+            GenerateBySideWinder();
+        }
+        public void Rendor()
+        {
+            ConsoleColor precolor = Console.ForegroundColor;
+
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    Console.ForegroundColor = GetTileColor(_tile[y, x]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = precolor;
+        }
+
+        ConsoleColor GetTileColor(Tiletype type)
+        {
+            switch (type)
+            {
+                case Tiletype.Empty:
+                    return ConsoleColor.Green;
+                case Tiletype.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+            }
+
+        }
+
+        public void GenerateByBinaryTree()
+        {
+            //길을 다 막아버리는 작업
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (y % 2 == 0 || x % 2 == 0)
+                    {
+                        _tile[y, x] = Tiletype.Wall;
+                    }
+                    else
+                    {
+                        _tile[y, x] = Tiletype.Empty;
+                    }
+                }
+            }
+
+            //랜덤으로 우측 혹은 아래로 길 뚫는 작업
+            //Binary Tree Algorithm
+            Random random = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (y % 2 == 0 || x % 2 == 0)
+                        continue;
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = Tiletype.Empty;
+                        continue;
+                    }
+
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = Tiletype.Empty;
+                        continue;
+                    }
+
+                    if (random.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = Tiletype.Empty;
+                    }
+                    else
+                    {
+                        _tile[y + 1, x] = Tiletype.Empty;
+                    }
+                }
+            }
+        }
+
+        public void GenerateBySideWinder()
+        {
+            //길을 다 막아버리는 작업
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (y % 2 == 0 || x % 2 == 0)
+                    {
+                        _tile[y, x] = Tiletype.Wall;
+                    }
+                    else
+                    {
+                        _tile[y, x] = Tiletype.Empty;
+                    }
+                }
+            }
+
+            //랜덤으로 우측 혹은 아래로 길 뚫는 작업
+            //Binary Tree Algorithm
+            Random random = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                int count = 1;
+                for (int x = 0; x < _size; x++)
+                {
+                    if (y % 2 == 0 || x % 2 == 0)
+                        continue;
+
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = Tiletype.Empty;
+                        continue;
+                    }
+
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = Tiletype.Empty;
+                        continue;
+                    }
+
+
+                    if (random.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = Tiletype.Empty;
+                        count++;
+                    }
+                    else
+                    {
+                        int randomIndex = random.Next(0, count);
+                        _tile[y + 1, x - randomIndex * 2] = Tiletype.Empty;
+                        count = 1;
+                    }
+                }
+            }
         }
     }
 }
--- Part2/Algorithm/Program.cs
+++ Part2/Algorithm/Program.cs
@@ -7,17 +7,17 @@ namespace Algorithm
         static void Main(string[] args)
         {
             Board board = new Board();
-            board.Initialize();
+            board.Initialize(25);
 
             Console.CursorVisible = false;
             //커서 끄기
 
 
             const int WAIT_TICK = 1000 / 30;
-            const char CIRCLE = '\u25cf';
+           
 
             int lastTick = 0;
-            
+
             while (true)
             {
                 #region 프레임 관리
@@ -33,18 +33,14 @@ namespace Algorithm
                 // 로직
 
                 // 렌더링
+                board.Rendor();
                 Console.SetCursorPosition(0, 0);
 
-                for(int i = 0; i < 25; i++)
-                {
-                    for(int j = 0; j < 25; j++)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(CIRCLE);
-                    }
-                    Console.WriteLine();
-                }
+
             }
+
+
+
         }
     }
 }
