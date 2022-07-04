d2ac7599a1fe7462b36f0a535f5e54f57223c270
Chanbae <dlcksqo01@gmail.com>
2022-07-04 21:47:59 +0900

    Stack, Queue

---
+++ Part2/Exercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise
+{
+    // 스택 : LIFO
+    // 큐 : FIFO
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(101);
+            stack.Push(102);
+            stack.Push(103);
+            stack.Push(104);
+            stack.Push(105);
+
+            int data = stack.Pop();
+            int data2 = stack.Peek();
+
+            
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(101);
+            queue.Enqueue(102);
+            queue.Enqueue(103);
+            queue.Enqueue(104);
+            queue.Enqueue(105);
+
+            int data3 = queue.Dequeue();
+            int data4 = queue.Peek();
+        }
+    }
+}
