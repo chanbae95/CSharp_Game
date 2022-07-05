cdc039897fd3d89e4a9f9e1f3e2ad1d2d1a5e7d8
Chanbae <dlcksqo01@gmail.com>
2022-07-05 20:35:55 +0900

    그래프셋팅

---
+++ Part2/Exercise2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Graph
+    {
+        int[,] adj = new int[6, 6]
+        {
+            { 0, 1, 0, 1, 0, 0 },
+            { 1, 0, 1, 1, 0, 0 },
+            { 1, 0, 0, 0, 0, 0 },
+            { 1, 1, 0, 0, 1, 0 },
+            { 0, 0, 0, 1, 0, 1 },
+            { 0, 0, 0, 0, 1, 0 },
+        };
+
+        List<int>[] adj2 = new List<int>[]
+        {
+            new List<int>(){ 1, 3},
+            new List<int>(){ 0, 2, 3},
+            new List<int>(){ 1 },
+            new List<int>(){ 0, 1, 4},
+            new List<int>(){ 3, 5},
+            new List<int>(){ 4 },
+        };
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DFS (깊이 우선 탐색)
+            //BFS (너비 우선 탐색)
+
+
+        }
+    }
+}
